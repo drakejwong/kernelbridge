ef99ee4311bf6fa5ce3bf7b19c6ffdd67880e5e7
Drake Wong <drakejwong@gmail.com>
2020-03-24 20:29:59 -0700

    Change Combination _kernels appendation, add unit tests for kernel creation, addition with constant

--- tests/test_kernelbridge.py
+++ tests/test_kernelbridge.py
@@ -6,8 +6,10 @@ import pytest
 
 from click.testing import CliRunner
 
-from kernelbridge import kernelbridge
+from kernelbridge import basekernel
 from kernelbridge import cli
+
+import numpy as np
 
 
 @pytest.fixture
@@ -24,6 +26,30 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
+
+def test_kernel_creation():
+    """Test basekernel creation of RBF kernel with full hyperparameter specs."""
+    basekernel.BaseKernel.create(
+        "RBF",
+        "Radial basis function kernel",
+        "exp(-0.5 * (x1 - x2) ** 2 * length_scale**-2)",
+        ('x1', 'x2'),
+        ('length_scale', np.float32, 1e-6, float("inf"),
+         "Determines rate of kernel decay to zero.")
+    )
+
+def test_kernel_addition():
+    """Test basekernel addition of stationary kernel with constant kernel."""
+    k1 = basekernel.BaseKernel.create(
+        "RBF",
+        "Radial basis function kernel",
+        "exp(-0.5 * (x1 - x2) ** 2 * length_scale**-2)",
+        ('x1', 'x2'),
+        ('length_scale', np.float32, 1e-6, float("inf"),
+        "Determines rate of kernel decay to zero.")
+    )
+    
+    basekernel.Sum(k1, 2)
 
 
 def test_command_line_interface():
